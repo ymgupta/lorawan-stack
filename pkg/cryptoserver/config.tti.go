2c3ce02f189c5188cb3c66f02d7e4d79e2fef1e4
Johan Stokking <johan@stokking.nl>
2019-01-21 22:23:42 +0100

    cs: Add Microchip crypto service provider to configuration

--- pkg/cryptoserver/config.tti.go
+++ pkg/cryptoserver/config.tti.go
@@ -3,30 +3,34 @@
 package cryptoserver
 
 import (
+	"context"
+
 	"go.thethings.network/lorawan-stack/pkg/crypto/cryptoservices"
-	"go.thethings.network/lorawan-stack/pkg/errors"
+	"go.thethings.network/lorawan-stack/pkg/cryptoserver/providers/microchip"
 )
 
 // Config represents the CryptoServer configuration.
 type Config struct {
-	NetworkService     string `name:"network-service" description:"Network crypto service to host"`
-	ApplicationService string `name:"application-service" description:"Application crypto service to host"`
+	NetworkService     string `name:"network-service" description:"Network crypto service to host (microchip)"`
+	ApplicationService string `name:"application-service" description:"Application crypto service to host (microchip)"`
+
+	Microchip microchip.Config `name:"microchip"`
 }
 
-var errServiceNotConfigured = errors.DefineFailedPrecondition("service_not_configured", "service `{id}` not configured")
-
-// Network returns the service for network-layer crypto operations.
-func (c *Config) Network() (cryptoservices.Network, error) {
-	if c.NetworkService == "" {
-		return nil, nil
+// NewNetwork returns a new service for network-layer crypto operations.
+func (c *Config) NewNetwork(ctx context.Context) (cryptoservices.Network, error) {
+	switch c.NetworkService {
+	case "microchip":
+		return microchip.New(ctx, &c.Microchip)
 	}
 	return nil, nil
 }
 
-// Application returns the service for application-layer crypto operations.
-func (c *Config) Application() (cryptoservices.Application, error) {
-	if c.ApplicationService == "" {
-		return nil, nil
+// NewApplication returns a new service for application-layer crypto operations.
+func (c *Config) NewApplication(ctx context.Context) (cryptoservices.Application, error) {
+	switch c.ApplicationService {
+	case "microchip":
+		return microchip.New(ctx, &c.Microchip)
 	}
 	return nil, nil
 }
