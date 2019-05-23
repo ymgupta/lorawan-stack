d78b18601bc415b5f3a8f358c77e892bb4c3ac4c
Hylke Visser <git@htdvisser.nl>
2019-05-23 10:58:37 +0200

    all: Make pkg/metrics tenant-aware

--- pkg/metrics/metrics.ttn.go
+++ pkg/metrics/metrics.ttn.go
@@ -12,28 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package metrics creates the metrics registry and exposes some common metrics.
+//+build !tti
+
 package metrics
 
 import (
+	"context"
+
 	"github.com/prometheus/client_golang/prometheus"
-	"go.thethings.network/lorawan-stack/pkg/version"
 )
 
-// Namespace for metrics.
-const Namespace = "ttn_lw"
+// ContextLabelNames are the label names that can be retrieved from a context for XXXVec metrics.
+var ContextLabelNames []string
 
-var ttnInfo = prometheus.NewGauge(prometheus.GaugeOpts{
-	Namespace: Namespace,
-	Name:      "info",
-	Help:      "Information about the TTN environment.",
-	ConstLabels: prometheus.Labels{
-		"version":    version.TTN,
-		"build_date": version.BuildDate,
-		"git_commit": version.GitCommit,
-	},
-})
-
-func init() {
-	ttnInfo.Set(1)
-}
+// LabelsFromContext returns the values for ContextLabelNames.
+var LabelsFromContext func(ctx context.Context) prometheus.Labels
