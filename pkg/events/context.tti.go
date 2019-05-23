dfaa0d1a3b1972d458af900a141a01bb6b3c9c9f
Hylke Visser <git@htdvisser.nl>
2019-05-23 13:25:15 +0200

    all: Make pkg/events tenant-aware

---
+++ pkg/events/context.tti.go
@@ -0,0 +1,36 @@
+// Copyright © 2019 The Things Industries B.V.
+
+//+build tti
+
+package events
+
+import (
+	"context"
+
+	"go.thethings.network/lorawan-stack/pkg/tenant"
+	"go.thethings.network/lorawan-stack/pkg/ttipb"
+)
+
+type tenantIDContextMarshaler struct{}
+
+func (tenantIDContextMarshaler) MarshalContext(ctx context.Context) []byte {
+	if id := tenant.FromContext(ctx); !id.IsZero() {
+		if b, err := id.Marshal(); err == nil {
+			return b
+		}
+	}
+	return nil
+}
+
+func (tenantIDContextMarshaler) UnmarshalContext(ctx context.Context, b []byte) (context.Context, error) {
+	var id ttipb.TenantIdentifiers
+	if err := id.Unmarshal(b); err != nil {
+		return nil, err
+	}
+	return tenant.NewContext(ctx, id), nil
+}
+
+func init() {
+	var m tenantIDContextMarshaler
+	RegisterContextMarshaler("tenant-id", m)
+}
