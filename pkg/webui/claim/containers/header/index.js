91745662413411818f49a0bb327e90c68ad0b952
Kevin Schiffer <mail@kevinschiffer.de>
2019-08-22 16:01:15 +0200

    dcs: Add initial frontend code

---
+++ pkg/webui/claim/containers/header/index.js
@@ -0,0 +1,86 @@
+// Copyright © 2019 The Things Network Foundation, The Things Industries B.V.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import bind from 'autobind-decorator'
+
+import { logout } from '../../store/actions/user'
+import PropTypes from '../../../lib/prop-types'
+import sharedMessages from '../../../lib/shared-messages'
+
+import HeaderComponent from '../../../components/header'
+
+@withRouter
+@connect(
+  state => ({
+    user: state.user.user,
+  }),
+  dispatch => ({
+    handleLogout: () => dispatch(logout()),
+  }),
+)
+@bind
+class Header extends Component {
+  static propTypes = {
+    /**
+     * The User object, retrieved from the API. If it is `undefined`, then the
+     * guest header is rendered
+     */
+    user: PropTypes.object,
+    /** Flag identifying whether links should be rendered as plain anchor link */
+    anchored: PropTypes.bool,
+    /** A handler for when the user used the search input */
+    handleSearchRequest: PropTypes.func,
+    /** A handler for when the user clicks the logout button */
+    handleLogout: PropTypes.func.isRequired,
+    /** A flag identifying whether the header should display the search input */
+    searchable: PropTypes.bool,
+  }
+
+  render() {
+    const { user, anchored, handleSearchRequest, handleLogout, searchable } = this.props
+
+    const navigationEntries = [
+      {
+        title: sharedMessages.applications,
+        icon: 'application',
+        path: '/',
+        exact: true,
+      },
+    ]
+
+    const dropdownItems = [
+      {
+        title: sharedMessages.logout,
+        icon: 'power_settings_new',
+        action: handleLogout,
+      },
+    ]
+
+    return (
+      <HeaderComponent
+        user={user}
+        dropdownItems={dropdownItems}
+        navigationEntries={navigationEntries}
+        anchored={anchored}
+        searchable={searchable}
+        handleSearchRequest={handleSearchRequest}
+      />
+    )
+  }
+}
+
+export default Header
