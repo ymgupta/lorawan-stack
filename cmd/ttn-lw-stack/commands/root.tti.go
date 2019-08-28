9f8b94d080c5acb3660fc0a35449b1bf0aff725a
Hylke Visser <hylke@thethingsindustries.com>
2019-08-28 17:14:20 +0200

    all: Add logging for license on start

--- cmd/ttn-lw-stack/commands/root.tti.go
+++ cmd/ttn-lw-stack/commands/root.tti.go
@@ -6,6 +6,7 @@ import (
 	"context"
 
 	pkglicense "go.thethings.network/lorawan-stack/pkg/license"
+	"go.thethings.network/lorawan-stack/pkg/log"
 	"go.thethings.network/lorawan-stack/pkg/ttipb"
 )
 
@@ -14,8 +15,15 @@ var license *ttipb.License
 func initializeLicense(ctx context.Context) (context.Context, error) {
 	var err error
 	license, err = pkglicense.Read(config.License)
-	if err == nil && license != nil {
-		ctx = pkglicense.NewContextWithLicense(ctx, *license)
+	if err != nil {
+		return nil, err
 	}
+	logger := log.FromContext(ctx)
+	if license == nil {
+		logger.Warn("No license configured, running in unlicensed mode")
+		return ctx, nil
+	}
+	logger.Info("Valid license")
+	ctx = pkglicense.NewContextWithLicense(ctx, *license)
 	return ctx, err
 }
