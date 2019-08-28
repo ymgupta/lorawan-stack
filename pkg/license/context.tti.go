28c76d09ffecf69ef6c73ab682e5d5fbae621dca
Hylke Visser <hylke@thethingsindustries.com>
2019-08-28 16:58:02 +0200

    all: Add metering to license package

--- pkg/license/context.tti.go
+++ pkg/license/context.tti.go
@@ -7,7 +7,6 @@ import (
 	"errors"
 	"fmt"
 	"strings"
-	"sync"
 	"time"
 
 	"go.thethings.network/lorawan-stack/pkg/ttipb"
@@ -18,11 +17,6 @@ import (
 type licenseContextKeyType struct{}
 
 var licenseContextKey licenseContextKeyType
-
-type licenseContext struct {
-	mu      sync.RWMutex
-	license *ttipb.License
-}
 
 var now = time.Now()
 
@@ -40,31 +34,18 @@ var defaultLicense = ttipb.License{
 
 // NewContextWithLicense returns a context derived from parent that contains license.
 func NewContextWithLicense(parent context.Context, license ttipb.License) context.Context {
-	return context.WithValue(parent, licenseContextKey, &licenseContext{license: &license})
+	return context.WithValue(parent, licenseContextKey, license)
 }
 
 // FromContext returns the License from the context if present. Otherwise it returns default License.
 func FromContext(ctx context.Context) ttipb.License {
-	if lc, ok := ctx.Value(licenseContextKey).(*licenseContext); ok {
-		lc.mu.RLock()
-		license := *lc.license
-		lc.mu.RUnlock()
+	if license, ok := ctx.Value(licenseContextKey).(ttipb.License); ok {
+		if license.Metering != nil {
+			license = globalMetering.Apply(license)
+		}
 		return license
 	}
 	return defaultLicense
-}
-
-// Mutate mutates the license in the context.
-func Mutate(ctx context.Context, update func(license ttipb.License) ttipb.License) {
-	lc, ok := ctx.Value(licenseContextKey).(*licenseContext)
-	if !ok {
-		panic("no license in context")
-	}
-	lc.mu.Lock()
-	license := *lc.license
-	license = update(license)
-	lc.license = &license
-	lc.mu.Unlock()
 }
 
 // RequireComponent requires components to be included in the license.
---
+++ pkg/license/metering.tti.go
@@ -0,0 +1,147 @@
+// Copyright © 2019 The Things Industries B.V.
+
+package license
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"go.thethings.network/lorawan-stack/pkg/ttipb"
+	"go.thethings.network/lorawan-stack/pkg/ttnpb"
+	"google.golang.org/grpc"
+)
+
+// Cluster is the interface used for getting metrics.
+type Cluster interface {
+	WithClusterAuth() grpc.CallOption
+	GetPeerConn(ctx context.Context, role ttnpb.ClusterRole) (*grpc.ClientConn, error)
+}
+
+// MeteringReporter is the interface used for reporting metrics.
+type MeteringReporter interface {
+	Report(ctx context.Context, data *ttipb.MeteringData) error
+}
+
+type meteringSetup struct {
+	config   *ttipb.MeteringConfiguration
+	cluster  Cluster
+	reporter MeteringReporter
+
+	mu    sync.RWMutex
+	apply func(ttipb.License) ttipb.License
+}
+
+// Apply updates the license according to the update rules.
+func (s *meteringSetup) Apply(license ttipb.License) ttipb.License {
+	if s == nil {
+		return license
+	}
+	s.mu.RLock()
+	if s.apply != nil {
+		license = s.apply(license)
+	}
+	s.mu.RUnlock()
+	return license
+}
+
+// CollectAndReport collects metrics from the cluster and reports them to the MeteringReporter.
+func (s *meteringSetup) CollectAndReport(ctx context.Context) error {
+	var meteringData ttipb.MeteringData
+
+	cc, err := s.cluster.GetPeerConn(ctx, ttnpb.ClusterRole_ENTITY_REGISTRY)
+	if err != nil {
+		return err
+	}
+
+	reg := ttipb.NewTenantRegistryClient(cc)
+	creds := s.cluster.WithClusterAuth()
+
+	// TODO: List all tenants and range over those.
+	// tenants, err := reg.List(ctx, &ttipb.ListTenantsRequest{
+	// 	FieldMask: pbtypes.FieldMask{Paths: []string{"ids"}},
+	// 	Limit:     0,
+	// 	Page:      1, // TODO: Pagination
+	// }, creds)
+	// if err != nil {
+	// 	return err
+	// }
+
+	totals, err := reg.GetRegistryTotals(ctx, &ttipb.GetTenantRegistryTotalsRequest{
+		// TODO: TenantIdentifiers
+	}, creds)
+	if err != nil {
+		return err
+	}
+	meteringData.Tenants = append(meteringData.Tenants, &ttipb.MeteringData_TenantMeteringData{
+		// TODO: TenantIdentifiers
+		Totals: totals,
+	})
+
+	if err = s.reporter.Report(ctx, &meteringData); err != nil {
+		return err
+	}
+
+	if s.config.OnSuccess != nil {
+		now := time.Now()
+		s.mu.Lock()
+		s.apply = func(license ttipb.License) ttipb.License {
+			if s.config.OnSuccess.ValidUntil != nil {
+				license.ValidUntil = now.Add(*s.config.OnSuccess.ValidUntil)
+			}
+			return license
+		}
+		s.mu.Unlock()
+	}
+
+	return nil
+}
+
+// Run the periodic metrics reporting.
+func (s *meteringSetup) Run(ctx context.Context) error {
+	interval := time.Hour
+	if s.config.Interval != nil {
+		interval = *s.config.Interval
+	}
+	reportTicker := time.NewTicker(interval)
+	defer reportTicker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return s.CollectAndReport(ctx)
+		case <-reportTicker.C:
+			s.CollectAndReport(ctx)
+		}
+	}
+}
+
+var globalMetering *meteringSetup
+
+// SetupMetering sets up metering on cluster.
+func SetupMetering(ctx context.Context, config *ttipb.MeteringConfiguration, cluster Cluster) error {
+	if globalMetering != nil {
+		return errors.New("only one metering configuration can be set up")
+	}
+	globalMetering = &meteringSetup{
+		config:  config,
+		cluster: cluster,
+	}
+	switch reporterConfig := config.Metering.(type) {
+	case *ttipb.MeteringConfiguration_Aws:
+		// TODO: Set up AWS metering reporter.
+		_ = reporterConfig
+		globalMetering.reporter = nil
+	default:
+		panic(fmt.Errorf("unsupported metering reporter config type: %T", config.Metering))
+	}
+
+	if err := globalMetering.CollectAndReport(ctx); err != nil {
+		return err
+	}
+
+	go globalMetering.Run(ctx)
+
+	return nil
+}
