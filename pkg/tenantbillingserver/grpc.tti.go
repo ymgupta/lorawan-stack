8d5d5ca28fc5a74c42f63bb0867257eb42cd48b4
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2019-11-01 16:54:15 +0100

    tbs: Do not fail reporting if a backend returns error

--- pkg/tenantbillingserver/grpc.tti.go
+++ pkg/tenantbillingserver/grpc.tti.go
@@ -6,6 +6,7 @@ import (
 	"context"
 
 	"github.com/gogo/protobuf/types"
+	"go.thethings.network/lorawan-stack/pkg/log"
 	"go.thethings.network/lorawan-stack/pkg/tenant"
 	"go.thethings.network/lorawan-stack/pkg/ttipb"
 	"go.thethings.network/lorawan-stack/pkg/ttnpb"
@@ -17,15 +18,19 @@ func (tbs *TenantBillingServer) Report(ctx context.Context, data *ttipb.Metering
 	if !ok {
 		panic("tenant fetcher not available")
 	}
+	logger := log.FromContext(ctx)
+outer:
 	for _, tenantData := range data.Tenants {
 		tenant, err := tenantFetcher.FetchTenant(ctx, &tenantData.TenantIdentifiers, "attributes", "state")
 		if err != nil {
-			return nil, err
+			logger.WithError(err).Error("Failed to retrieve tenant")
+			break outer
 		}
 		for _, backend := range tbs.backends {
 			err := backend.Report(ctx, tenant, tenantData.Totals)
 			if err != nil {
-				return nil, err
+				logger.WithError(err).Error("Failed to report metrics to backend")
+				break outer
 			}
 		}
 	}
