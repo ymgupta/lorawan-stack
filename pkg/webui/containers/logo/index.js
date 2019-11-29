95b50f19b58484b63ea18cd1af58091f7a9e565d
Kevin Schiffer <mail@kevinschiffer.de>
2019-11-29 16:40:08 +0100

    console,oauth: Add branding text to logo container

--- pkg/webui/containers/logo/index.js
+++ pkg/webui/containers/logo/index.js
@@ -19,12 +19,14 @@ import LogoComponent from '../../components/logo'
 import {
   selectAssetsRootPath,
   selectBrandingRootPath,
+  selectBrandingText,
+  selectBrandingClusterID,
   selectApplicationSiteName,
 } from '../../lib/selectors/env'
 
 const logo = {
   src: `${selectAssetsRootPath()}/ttes.svg`,
-  alt: `${selectApplicationSiteName} Logo`,
+  alt: `${selectApplicationSiteName()} Logo`,
 }
 const hasCustomBranding = selectBrandingRootPath() !== selectAssetsRootPath()
 const secondaryLogo = hasCustomBranding
@@ -34,6 +36,14 @@ const secondaryLogo = hasCustomBranding
     }
   : undefined
 
-const Logo = props => <LogoComponent logo={logo} secondaryLogo={secondaryLogo} {...props} />
+const Logo = props => (
+  <LogoComponent
+    logo={logo}
+    secondaryLogo={secondaryLogo}
+    {...props}
+    text={selectBrandingText()}
+    clusterTag={selectBrandingClusterID()}
+  />
+)
 
 export default Logo
