5a6c1737fccf1fe8f90e91696cdb8d59f630cd0c
Adrian-Stefan Mares <adrian.mares@thethingsnetwork.org>
2020-01-13 13:51:29 +0100

    as: Derive downlink operations URL using the tenant

---
+++ pkg/applicationserver/io/web/webhooks.tti.go
@@ -0,0 +1,38 @@
+// Copyright © 2019 The Things Industries B.V.
+
+package web
+
+import (
+	"context"
+	"net/url"
+
+	"go.thethings.network/lorawan-stack/pkg/license"
+	"go.thethings.network/lorawan-stack/pkg/tenant"
+)
+
+// Apply the context to the data.
+func (c DownlinksConfig) Apply(ctx context.Context) DownlinksConfig {
+	if license.RequireMultiTenancy(ctx) != nil {
+		return c
+	}
+	tenantID := tenant.FromContext(ctx).TenantID
+	if tenantID == "" {
+		return c
+	}
+	deriv := c
+	if c.PublicAddress != "" {
+		base, err := url.Parse(c.PublicAddress)
+		if err == nil {
+			base.Host = tenantID + "." + base.Host
+			deriv.PublicAddress = base.String()
+		}
+	}
+	if c.PublicTLSAddress != "" {
+		base, err := url.Parse(c.PublicTLSAddress)
+		if err == nil {
+			base.Host = tenantID + "." + base.Host
+			deriv.PublicTLSAddress = base.String()
+		}
+	}
+	return deriv
+}
