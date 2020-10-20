97568e2a45fd51eb427515c670f08d400ece7b21
Kevin Schiffer <kevin.schiffer@thethingsnetwork.org>
2020-10-20 22:20:08 +0100

    console: Add redux primitives for pausing events

--- pkg/webui/console/store/index.js
+++ pkg/webui/console/store/index.js
@@ -29,7 +29,17 @@ import requestPromiseMiddleware from './middleware/request-promise-middleware'
 import logics from './middleware/logics'
 
 const composeEnhancers = (dev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-let middlewares = [requestPromiseMiddleware, createLogicMiddleware(logics)]
+
+const userTimingMiddleware = () => next => action => {
+  if (performance.mark === undefined) return next(action)
+  performance.mark(`${action.type}_start`)
+  const result = next(action)
+  performance.mark(`${action.type}_end`)
+  performance.measure(`${action.type}`, `${action.type}_start`, `${action.type}_end`)
+  return result
+}
+
+let middlewares = [requestPromiseMiddleware, createLogicMiddleware(logics), userTimingMiddleware]
 
 if (env.sentryDsn) {
   middlewares = [
